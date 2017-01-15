9a7dcb58a406e9018407151d346e947b4df3f8eb
Jessica Felts <thejessicafelts@gmail.com>
2017-01-15 10:53:49 -0500

    Update to tweets.js

--- assets/js/tweets.js
+++ assets/js/tweets.js
@@ -2,6 +2,8 @@ var tweetCounter = 0;
 var tweetsContainer = document.getElementById("potus-tweets");
 var btn = document.getElementById("btn");
 
+
+
 btn.addEventListener("click", function(){
   var request = new XMLHttpRequest();
   request.open('GET', 'assets/js/potus.json');
@@ -13,16 +15,16 @@ btn.addEventListener("click", function(){
 });
 
 function loadTweets(data){
+
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var date = new Date(data[tweetCounter].timestamp);
+  var year = date.getFullYear();
+  var month = date.getMonth();
+  var day = date.getDate();
+
   var tweet = "";
 
   for (i = 0; i < 1; i++){
-
-    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var date = new Date(data[tweetCounter].timestamp);
-    var year = date.getFullYear();
-    var month = date.getMonth();
-    var day = date.getDate();
-
     tweet += '<div class="tweet"><p class="copy">' + data[tweetCounter].text + '</p><p class="date">' + monthNames[month] + ' ' + day + ', ' + year + '</p></div>';
     tweetCounter++;
   };
